241ab53842a740e2166a2d16b9dead9548c6804c
Carl Unsworth <carlunsworthuk@gmail.com>
2016-05-12 16:18:08 +0100

    * MCNP setup in LinuxMint VBox * cheat colours on Macbook * MatLAb startup script added

---
+++ Other/matlab_startup.m
@@ -0,0 +1,8 @@
+disp('Custom startup script running...')
+
+% Add folder with useful scripts to path
+addpath('/Users/cu/Code/MATLAB/MATLAB_Functions')
+addpath('/Users/cu/Code/MATLAB/FitTools')
+
+% Alias useful built-in functions
+n2s = @num2str;
